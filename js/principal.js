c73c905cab58092febe5de4fb3125cedd34ef4e6
Rafhael1901 <140663087+Rafhael1901@users.noreply.github.com>
2024-09-17 08:28:39 -0300

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,8 @@
 var titulo = document.querySelector('h1');
 titulo.textContent = 'Aparecida Nutricionista';   
+var paulo = document.querySelector('#Paulo');
+var peso = paulo.querySelector('.info-peso').textContent;
+var altura = paulo.querySelector('.info-altura').textContent;
+
+var imc = peso / (altura * altura);
+paulo.querySelector('.info-imc').textContent = imc;
