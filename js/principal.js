be3788bd66ab93dcc105d7a3e0f41dd51a95d4d8
Rafhael1901 <140663087+Rafhael1901@users.noreply.github.com>
2024-09-23 11:49:44 -0300

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -6,3 +6,12 @@ var altura = paulo.querySelector('.info-altura').textContent;
 
 var imc = peso / (altura * altura);
 paulo.querySelector('.info-imc').textContent = imc;
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for(let contador = 0; contador < pacientes.length; contador++){
+    var peso = pacientes[contador].querySelector('.info-peso').textContent;
+    var altura = pacientes[contador].querySelector('.info-altura').textContent;
+    var imc = peso / (altura * altura);
+    pacientes[contador].querySelector('.info-imc').textContent = imc;
+}
